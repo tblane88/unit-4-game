caf03e8ff3fe2e6ccf84d1a9542d0606beecb0e8
tblane <tblane88@gmail.com>
2018-08-15 16:44:31 -0400

    Added images, added computers guess and users score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-    alert('hi');
+//    Set the global variables
+    // set the computer guess between 19 and 120
+    var computerGuess = Math.floor(Math.random() * 101) + 19 
+    // set user number to zero initially
+    var usersNumber = 0
+
+
+
+    // Display the Computer's guess
+    $("#computerNumber").text(computerGuess);
+    $("#userNumber").text(usersNumber);
 
 
 });
