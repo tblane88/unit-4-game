7b6e49f7c28c01c5781185abc55f15169cf13661
tblane <tblane88@gmail.com>
2018-08-18 17:15:10 -0400

    added defenderId and heroAttack and DefenderAttack

--- assets/javascript/hiddenGame.js
+++ assets/javascript/hiddenGame.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     // global variables
     var opponentPicked = false;
     var heroPicked = false;
+    var hero = "";
+    var defender = "";
+    var heroScore = 0;
+    var defenderScore = 0;
+    var heroAttack = 0;
+    var defenderAttack = 0;
+    var defenderID = "";
+
+
+
 
 
 
@@ -17,10 +27,14 @@ $(document).ready(function() {
             $("#RickOpponent").removeClass("hidden");
             $("#MalcolmOpponent").removeClass("hidden");
             $("#DoctorOpponent").removeClass("hidden");
+
+            // set hero name
+            hero = "Han Solo";
+            heroScore = 120;
+            heroAttack = 8;
             
             heroPicked = true;
        };
-
    });
 
    $("#RickHero").on("click", function () {
@@ -34,6 +48,11 @@ $(document).ready(function() {
             $("#HanOpponent").removeClass("hidden");
             $("#MalcolmOpponent").removeClass("hidden");
             $("#DoctorOpponent").removeClass("hidden");
+
+            // set hero name
+            hero = "Rick Sanchez";
+            heroScore = 100;
+            heroAttack = 10;
 
             heroPicked = true;
 
@@ -53,10 +72,15 @@ $(document).ready(function() {
             $("#HanOpponent").removeClass("hidden");
             $("#DoctorOpponent").removeClass("hidden");
 
+            // set hero name
+            hero = "Captain Mal Reynolds";
+            heroScore = 150;
+            heroAttack = 6;
+
             heroPicked = true;
 
        };
-
+       
    });
 
    $("#DoctorHero").on("click", function () {
@@ -71,9 +95,14 @@ $(document).ready(function() {
             $("#MalcolmOpponent").removeClass("hidden");
             $("#HanOpponent").removeClass("hidden");
 
+            // set hero name
+            hero = "The Doctor";
+            heroScore = 180;
+            heroAttack = 7;
+
             heroPicked = true;
 
-       }
+       };
 
    });
 
@@ -83,15 +112,28 @@ $(document).ready(function() {
         if (!opponentPicked) {
             $("#HanDefender").removeClass("hidden");
             $("#HanOpponent").addClass("hidden");
+
+            // set Defender
+            defender = "Han Solo";
+            defenderScore = 120;
+            defenderID = "#HanDefender";
+            defenderAttack = 8;
+
             opponentPicked = true;
         };
-
    });
 
    $("#RickOpponent").on("click", function () {
         if (!opponentPicked) {
             $("#RickDefender").removeClass("hidden");
             $("#RickOpponent").addClass("hidden");
+
+            // set Defender
+            defender = "Rick Sanchez";
+            defenderScore = 100;
+            defenderID = "#RickDefender";
+            defenderAttack = 5;
+
             opponentPicked = true;
         };
 
@@ -101,6 +143,13 @@ $(document).ready(function() {
         if (!opponentPicked) {
             $("#MalcolmDefender").removeClass("hidden");
             $("#MalcolmOpponent").addClass("hidden");
+
+            // set Defender
+            defender = "Captain Mal Reynolds";
+            defenderScore = 150;
+            defenderID = "#MalcolmDefender";
+            defenderAttack = 15;
+
             opponentPicked = true;
         };
 
@@ -110,14 +159,29 @@ $(document).ready(function() {
         if (!opponentPicked) {
             $("#DoctorDefender").removeClass("hidden");
             $("#DoctorOpponent").addClass("hidden");
+
+            // set Defender
+            defender = "The Doctor";
+            defenderScore = 180;
+            defenderID = "#DoctorDefender";
+            defenderAttack = 20;
+
             opponentPicked = true;
         };
+        
 
     });
 
 
 
+    $("#attack").on("click", function() {
+        if ((heroPicked) && (opponentPicked)) {
+            $(defenderID).addClass("hidden");
 
+        };
+
+
+    });
 
 
 
