3fb58f984d332ca27f379ee4ef06f3cb73db6227
tblane <tblane88@gmail.com>
2018-08-18 09:49:33 -0400

    added more functionality to the images.

--- assets/javascript/hiddenGame.js
+++ assets/javascript/hiddenGame.js
@@ -1,58 +1,119 @@
 $(document).ready(function() {
-   $("#HanHero").on("click", function () {
-    //    hide the other heros
-        $("#RickHero").addClass("hidden");
-        $("#MalcolmHero").addClass("hidden");
-        $("#DoctorHero").addClass("hidden");
+    // global variables
+    var opponentPicked = false;
+    var heroPicked = false;
 
-        // unhide the opponents
-        $("#RickOpponent").removeClass("hidden");
-        $("#MalcolmOpponent").removeClass("hidden");
-        $("#DoctorOpponent").removeClass("hidden");
+
+
+    // initial click me for heros
+   $("#HanHero").on("click", function () {
+       if (!heroPicked) {
+        //    hide the other heros
+            $("#RickHero").addClass("hidden");
+            $("#MalcolmHero").addClass("hidden");
+            $("#DoctorHero").addClass("hidden");
+
+            // unhide the opponents
+            $("#RickOpponent").removeClass("hidden");
+            $("#MalcolmOpponent").removeClass("hidden");
+            $("#DoctorOpponent").removeClass("hidden");
+            
+            heroPicked = true;
+       };
 
    });
 
    $("#RickHero").on("click", function () {
-    //    hide the other heros
-        $("#HanHero").addClass("hidden");
-        $("#MalcolmHero").addClass("hidden");
-        $("#DoctorHero").addClass("hidden");
+       if (!heroPicked) {
+        //    hide the other heros
+            $("#HanHero").addClass("hidden");
+            $("#MalcolmHero").addClass("hidden");
+            $("#DoctorHero").addClass("hidden");
 
-        // unhide the opponents
-        $("#HanOpponent").removeClass("hidden");
-        $("#MalcolmOpponent").removeClass("hidden");
-        $("#DoctorOpponent").removeClass("hidden");
+            // unhide the opponents
+            $("#HanOpponent").removeClass("hidden");
+            $("#MalcolmOpponent").removeClass("hidden");
+            $("#DoctorOpponent").removeClass("hidden");
+
+            heroPicked = true;
+
+       };
 
    });
 
    $("#MalcolmHero").on("click", function () {
-    //    hide the other heros
-        $("#RickHero").addClass("hidden");
-        $("#HanHero").addClass("hidden");
-        $("#DoctorHero").addClass("hidden");
+       if (!heroPicked) {
+        //    hide the other heros
+            $("#RickHero").addClass("hidden");
+            $("#HanHero").addClass("hidden");
+            $("#DoctorHero").addClass("hidden");
 
-        // unhide the opponents
-        $("#RickOpponent").removeClass("hidden");
-        $("#HanOpponent").removeClass("hidden");
-        $("#DoctorOpponent").removeClass("hidden");
+            // unhide the opponents
+            $("#RickOpponent").removeClass("hidden");
+            $("#HanOpponent").removeClass("hidden");
+            $("#DoctorOpponent").removeClass("hidden");
+
+            heroPicked = true;
+
+       };
 
    });
 
    $("#DoctorHero").on("click", function () {
-    //    hide the other heros
-        $("#RickHero").addClass("hidden");
-        $("#MalcolmHero").addClass("hidden");
-        $("#HanHero").addClass("hidden");
+       if (!heroPicked) {
+        //    hide the other heros
+            $("#RickHero").addClass("hidden");
+            $("#MalcolmHero").addClass("hidden");
+            $("#HanHero").addClass("hidden");
 
-        // unhide the opponents
-        $("#RickOpponent").removeClass("hidden");
-        $("#MalcolmOpponent").removeClass("hidden");
-        $("#HanOpponent").removeClass("hidden");
+            // unhide the opponents
+            $("#RickOpponent").removeClass("hidden");
+            $("#MalcolmOpponent").removeClass("hidden");
+            $("#HanOpponent").removeClass("hidden");
+
+            heroPicked = true;
+
+       }
 
    });
 
 
+//    opponent click handlers
+   $("#HanOpponent").on("click", function () {
+        if (!opponentPicked) {
+            $("#HanDefender").removeClass("hidden");
+            $("#HanOpponent").addClass("hidden");
+            opponentPicked = true;
+        };
 
+   });
+
+   $("#RickOpponent").on("click", function () {
+        if (!opponentPicked) {
+            $("#RickDefender").removeClass("hidden");
+            $("#RickOpponent").addClass("hidden");
+            opponentPicked = true;
+        };
+
+    });
+
+    $("#MalcolmOpponent").on("click", function () {
+        if (!opponentPicked) {
+            $("#MalcolmDefender").removeClass("hidden");
+            $("#MalcolmOpponent").addClass("hidden");
+            opponentPicked = true;
+        };
+
+   });
+
+   $("#DoctorOpponent").on("click", function () {
+        if (!opponentPicked) {
+            $("#DoctorDefender").removeClass("hidden");
+            $("#DoctorOpponent").addClass("hidden");
+            opponentPicked = true;
+        };
+
+    });
 
 
 
